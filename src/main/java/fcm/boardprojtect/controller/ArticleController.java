520d3c9eef1c641157df4755b6e94c42b913a024
sagong0 <1w2q3e@naver.com>
2024-06-10 10:20:50 +0900

    #16 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.

---
+++ src/main/java/fcm/boardprojtect/controller/ArticleController.java
@@ -0,0 +1,14 @@
+package fcm.boardprojtect.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * /articles/search	게시판 검색 전용 페이지
+ * /articles/search-hashtag	게시판 해시태그 검색 전용 페이지
+ */
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
