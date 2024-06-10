dc337b11d4a33debc2e776c0cd2dc361a406cec7
sagong0 <1w2q3e@naver.com>
2024-06-10 10:57:24 +0900

    #16 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로  테스트를 통과하게 열어주고,
    뷰 적절히 뼈대 잡음.
    테스트에서는 mediaType 검사에서 부가적인 옵션이 들어갈 수 있을을 확인했기 때문에, 적절히 테스트 메서드 수정

--- src/main/java/fcm/boardprojtect/controller/ArticleController.java
+++ src/main/java/fcm/boardprojtect/controller/ArticleController.java
@@ -1,7 +1,11 @@
 package fcm.boardprojtect.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * /articles/search	게시판 검색 전용 페이지
@@ -11,4 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+    @GetMapping
+    public String articles(ModelMap map){
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 }
