cbda012f0bd6e7a439a97cbf03a21077fd927504
sagong0 <1w2q3e@naver.com>
2024-06-10 11:22:29 +0900

    #16 - 컨트롤러 테스트 업데이트
    미디어 타입 검사 방법을 dc337b1 에서 확인하였기 때문에, 나머지 뷰 테스트도 동일 하게 작동 할 것으로 예상하고 미리 테스트 코드 업데이트

--- src/test/java/fcm/boardprojtect/controller/ArticleControllerTest.java
+++ src/test/java/fcm/boardprojtect/controller/ArticleControllerTest.java
@@ -42,7 +42,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -58,7 +58,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
     @Disabled("구현 중")
@@ -71,7 +71,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search-hashtag"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 }
 
