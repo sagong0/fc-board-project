ccc339f358fc23b15a72886a8cd29c52efaaf9e8
sagong0 <1w2q3e@naver.com>
2024-06-10 14:21:04 +0900

    Revert "#16 - 게시글 뷰 기본 골격 작성"
    This reverts commit 8c6a5b3ca786cb7e493b0e69adb78f89d5895673.

--- src/main/java/fcm/boardprojtect/controller/ArticleController.java
+++ src/main/java/fcm/boardprojtect/controller/ArticleController.java
@@ -3,7 +3,6 @@ package fcm.boardprojtect.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,12 +19,5 @@ public class ArticleController {
     public String articles(ModelMap map){
         map.addAttribute("articles", List.of());
         return "articles/index";
-    }
-
-    @GetMapping("/{articleId}")
-    public String article(@PathVariable Long articleId, ModelMap map){
-        map.addAttribute("article", "articledummy");    // TODO: 실제 데이터를 구현 할때 넣어줄 것.!!
-        map.addAttribute("articleComments", List.of());
-        return "articles/detail";
     }
 }
