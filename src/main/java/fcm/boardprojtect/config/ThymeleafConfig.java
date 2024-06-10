b92f14413e0f4b4f31d4926e4584e3fff811b849
sagong0 <1w2q3e@naver.com>
2024-06-10 13:34:42 +0900

    #16 - Tymeleaf3 decoupled logic 설정
    java code 로 thmeleaf3 decoupled logic 을
    사용자 application property 로 제어 할 수 있게
    설정 추가
    
    * https://gist.github.com/djkeh/6e1d557ce8c466135b1541d342b1c25c
    6ba08c

---
+++ src/main/java/fcm/boardprojtect/config/ThymeleafConfig.java
@@ -0,0 +1,29 @@
+package fcm.boardprojtect.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(
+            SpringResourceTemplateResolver defaultTemplateResolver,
+            Thymeleaf3Properties thymeleaf3Properties
+    ) {
+        defaultTemplateResolver.setUseDecoupledLogic(thymeleaf3Properties.decoupledLogic());
+
+        return defaultTemplateResolver;
+    }
+
+
+    /**
+     * Use Thymeleaf 3 Decoupled Logic
+     */
+    @ConfigurationProperties("spring.thymeleaf3")
+    public record Thymeleaf3Properties(boolean decoupledLogic) {}
+
+}
